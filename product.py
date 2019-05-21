1dde7688c98d5a4447079f9cea6c58d1e0f193c0
felcygrace <47312394+felcygrace@users.noreply.github.com>
2019-05-21 06:35:28 -0700

    Create product.py

---
+++ product.py
@@ -0,0 +1,9 @@
+def find_product(num1,num2,num3):
+    product=0
+    product=num1*num2*num3
+    #write your logic here
+    return product
+
+#Provide different values for num1, num2, num3 and test your program
+product=find_product(7,6,2)
+print(product)
